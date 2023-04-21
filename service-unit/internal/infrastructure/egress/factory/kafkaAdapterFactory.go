30e7de56780b7ab5aae959b01e34d0ea9b408560
Hiroki <hiroki11hanada@gmail.com>
2023-04-21 14:08:25 +0900

    service-unit:feat: consistent naming for kafka

--- service-unit/internal/infrastructure/egress/factory/kafkaAdapterFactory.go
+++ service-unit/internal/infrastructure/egress/factory/kafkaAdapterFactory.go
@@ -4,14 +4,14 @@ import (
 	"errors"
 
 	"github.com/hanapedia/the-bench/service-unit/internal/domain/core"
-	kafkaAdapter "github.com/hanapedia/the-bench/service-unit/internal/infrastructure/egress/producer_adapter/kafka"
+	"github.com/hanapedia/the-bench/service-unit/internal/infrastructure/egress/producer_adapter/kafka"
 )
 
 func (egressAdapterDetails EgressAdapterDetails) kafkaEgressAdapterFactory() (core.EgressAdapter, error) {
 	var kafkaEgressAdapter core.EgressAdapter
 	var err error
-	if kafkaWriterConnection, ok := (egressAdapterDetails.connection).(kafkaAdapter.KafkaWriterConnection); ok {
-		kafkaEgressAdapter = kafkaAdapter.KafkaProducerAdapter{Writer: kafkaWriterConnection.Connection}
+	if kafkaProducerConnection, ok := (egressAdapterDetails.connection).(kafka.KafkaProducerConnection); ok {
+		kafkaEgressAdapter = kafka.KafkaProducerAdapter{Writer: kafkaProducerConnection.Connection}
 	} else {
 		err = errors.New("Unmatched connection instance")
 	}
