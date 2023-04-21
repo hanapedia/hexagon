95c32bf80149ac64f75bd9f9286ea43d801c0045
Hiroki <hiroki11hanada@gmail.com>
2023-04-22 00:03:31 +0900

    stateful-unit:feat: mongo implemented

--- stateful-unit/mongo/import-data.sh
+++ stateful-unit/mongo/import-data.sh
@@ -8,7 +8,7 @@ done
 
 # Import the data into the database and collection of your choice
 mongoimport --jsonArray --db mongo --collection small --file /data/small.json
-mongoimport --jsonarray --db mongo --collection medium --file /data/medium.json
+mongoimport --jsonArray --db mongo --collection medium --file /data/medium.json
 mongoimport --jsonArray --db mongo --collection large --file /data/large.json
 
 echo "Data import completed."
