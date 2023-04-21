9ddb7fb84e3575b8d2aa697f8e80c6be24864c5b
Hiroki <hiroki11hanada@gmail.com>
2023-04-21 13:14:00 +0900

    stateful-unit:feat: custom mongo image with initial data

---
+++ stateful-unit/mongo/import-data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Wait for MongoDB to start
+until mongo --eval "print(\"waited for connection\")"; do
+  sleep 1
+done
+
+# Import the data into the database and collection of your choice
+mongoimport --jsonArray --db mongo --collection small --file /data/small.json
+mongoimport --jsonarray --db mongo --collection medium --file /data/medium.json
+mongoimport --jsonArray --db mongo --collection large --file /data/large.json
+
+echo "Data import completed."
