1e0538080f9b4f9df409d3b9de51f9ad7a8400b1
Hiroki <hiroki11hanada@gmail.com>
2023-05-26 21:23:32 +0900

    tbctl: deprecate old validation code

--- tbctl/pkg/kubernetes/templates/stateless_env.go
+++
@@ -1,8 +0,0 @@
-package templates
-
-const StatelessEnvs =
-`HTTP_PORT={{ .HTTP_PORT }}
-GRPC_PORT={{ .GRPC_PORT }}
-KAFKA_PORT={{ .KAFKA_PORT }}
-MONGO_PORT={{ .MONGO_PORT }}
-POSTGRE_PORT={{ .POSTGRE_PORT }}`
