1df86d34e3685604bc6027db9f74b57689943a4b
Hiroki <hiroki11hanada@gmail.com>
2023-05-29 00:39:00 +0900

    service-unit: init tracer

--- service-unit/stateless/internal/infrastructure/telemetry/tracing/tracing.go
+++ service-unit/stateless/internal/infrastructure/telemetry/tracing/tracing.go
@@ -5,22 +5,29 @@ import (
 
 	"github.com/hanapedia/the-bench/the-bench-operator/pkg/logger"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-func InitTracer(name string) {
+func InitTracer(name, collectorUrl string) *sdktrace.TracerProvider {
 	ctx := context.Background()
 
-	// Set up a trace provider
-	traceExporter, err := otlptrace.New(
-		ctx,
-		otlptracegrpc.NewClient(),
+	conn, err := grpc.DialContext(ctx, collectorUrl,
+		// Note the use of insecure transport here. TLS is recommended in production.
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
 	)
+	if err != nil {
+		logger.Logger.Fatalf("failed to create gRPC connection to collector: %w", err)
+	}
+
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
 	if err != nil {
 		logger.Logger.Fatalf("failed to create trace exporter: %v", err)
 	}
@@ -35,9 +42,16 @@ func InitTracer(name string) {
 		sdktrace.WithResource(resource),
 		sdktrace.WithSampler(sdktrace.TraceIDRatioBased(1)),
 	)
-
 	otel.SetTracerProvider(tracerProvider)
 
 	// Set the global text map propagator to tracecontext.
 	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	// return provider so that it can be shutdown
+	return tracerProvider
+}
+
+// get the default tracer for creating original spans
+func GetTracer() trace.Tracer {
+	return otel.Tracer("the-bench")
 }
