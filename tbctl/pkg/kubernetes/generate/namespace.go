7d302a0afd13d1b158605f74bef45774bc7466eb
Hiroki <hiroki11hanada@gmail.com>
2023-07-21 21:41:59 +0900

    tbctl: add namespace object definitions

---
+++ tbctl/pkg/kubernetes/generate/namespace.go
@@ -0,0 +1,33 @@
+package generate
+
+import (
+	"github.com/hanapedia/the-bench/the-bench-operator/pkg/object/cluster"
+	"github.com/hanapedia/the-bench/the-bench-operator/pkg/yaml"
+)
+
+// GenerateNamespaceManifests generates namespace manifest
+func (mg ManifestGenerator) GenerateNamespaceManifests() ManifestErrors {
+	// Open the manifestFile in append mode and with write-only permissions
+	outPath := mg.getFilePath(mg.ServiceUnitConfig.Name, "namespace")
+	manifestFile, err := createFile(outPath)
+	if err != nil {
+		return ManifestErrors{
+			common: []CommonManifestError{
+				NewCommonManifestError(mg.ServiceUnitConfig, "Unable to open output file."),
+			},
+		}
+	}
+	defer manifestFile.Close()
+
+	namespace := cluster.CreateNamespace()
+	namespaceYAML := yaml.GenerateManifest(namespace)
+	_, err = manifestFile.WriteString(formatManifest(namespaceYAML))
+	if err != nil {
+		return ManifestErrors{
+			common: []CommonManifestError{
+				NewCommonManifestError(mg.ServiceUnitConfig, "Failed to write namespace manifest"),
+			},
+		}
+	}
+	return ManifestErrors{}
+}
--- the-bench-operator/pkg/object/factory/namespace.go
+++ the-bench-operator/pkg/object/factory/namespace.go
@@ -6,14 +6,13 @@ import (
 
 type NamespaceArgs struct {
 	Name        string
-	Namespace   string
 	Annotations map[string]string
 }
 
 // NamespaceFactory create type namespace kubernetes objects.
-func NamespaceFactory(args NamespaceArgs) corev1.Namespace {
+func NamespaceFactory(args *NamespaceArgs) corev1.Namespace {
 	return corev1.Namespace{
 		TypeMeta:   TypeMetaFactory("Namespace", "v1"),
-		ObjectMeta: ObjectMetaFactory(ObjectMetaOptions{Name: args.Name, Namespace: args.Namespace, Annotations: args.Annotations}),
+		ObjectMeta: ObjectMetaFactory(ObjectMetaOptions{Name: args.Name, Annotations: args.Annotations}),
 	}
 }
