9e693c6e4b571600f54ba28d5cb80552ec4efc06
Hiroki <hiroki11hanada@gmail.com>
2023-11-15 02:00:20 +0900

    fix redis secodary

--- internal/service-unit/infrastructure/adapters/secondary/repository/redis/factory.go
+++ internal/service-unit/infrastructure/adapters/secondary/repository/redis/factory.go
@@ -15,15 +15,16 @@ func RedisClientAdapterFactory(adapterConfig *model.RepositoryClientConfig, clie
 		switch adapterConfig.Action {
 		case constants.READ:
 			redisAdapter = &redisReadAdapter{
-				name:   adapterConfig.Name,
-				client: redisClient.Client,
-				size:   adapterConfig.Payload,
+				name:           adapterConfig.Name,
+				client:         redisClient.Client,
+				payloadVariant: model.GetPayloadVariant(adapterConfig.Payload),
 			}
 		case constants.WRITE:
 			redisAdapter = &redisWriteAdapter{
-				name:   adapterConfig.Name,
-				client: redisClient.Client,
-				size:   adapterConfig.Payload,
+				name:           adapterConfig.Name,
+				client:         redisClient.Client,
+				payloadSize:    model.GetPayloadSize(adapterConfig.Payload),
+				payloadVariant: model.GetPayloadVariant(adapterConfig.Payload),
 			}
 		default:
 			err = errors.New("No matching action found when creating redis client adapter.")
