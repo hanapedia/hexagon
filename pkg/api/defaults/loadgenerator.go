96f11d08b086cebe9b128d03ab6cfdd2fec0cef8
Hiroki <hiroki11hanada@gmail.com>
2023-12-05 19:59:14 +0900

    export k6 metrics

--- pkg/api/defaults/loadgenerator.go
+++ pkg/api/defaults/loadgenerator.go
@@ -1,6 +1,8 @@
 package defaults
 
 const (
-	LOAD_GENERATOR_IMAGE_NAME = "hiroki11hanada/tb-load-generator"
-	LIMIT_MEM_LG = "1Gi"
+	LOAD_GENERATOR_IMAGE_NAME       = "hiroki11hanada/tb-load-generator"
+	LIMIT_MEM_LG                    = "1Gi"
+	LG_K6_PROMETHEUS_RW_SERVER_URL  = "http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090/api/v1/write"
+	LG_K6_PROMETHEUS_RW_TREND_STATS = "p(95),p(99),avg"
 )
