354f44c616653c822cf7669f77e0cfab71ff3066
Hiroki <hiroki11hanada@gmail.com>
2024-04-24 00:54:48 +0900

    test: implement mock for primary and secondary adapters

--- test/mock/primary.go
+++ test/mock/primary.go
@@ -1,1 +1,33 @@
 package mock
+
+import "github.com/hanapedia/hexagon/internal/service-unit/application/ports"
+
+type PrimaryAdapterMock struct {
+	addr string
+}
+
+// Serve mock implementation
+func (pam PrimaryAdapterMock) Serve() error {
+	return nil
+}
+
+// Register mock implementation
+func (pam PrimaryAdapterMock) Register(primaryHander *ports.PrimaryHandler) error {
+	return nil
+}
+
+// NewPrimaryHandler returns mocked ports.PrimaryHandler with given number of tasks
+func NewPrimaryHandler(numTask int) ports.PrimaryHandler {
+	tasks := make([]ports.Task, numTask)
+	for i := 0; i < numTask; i++ {
+		tasks = append(tasks, ports.Task{SecondaryPort: NewSecondaryAdapter(), Concurrent: false})
+	}
+	return ports.PrimaryHandler{
+		TaskSet: tasks,
+	}
+}
+
+// NewPrimaryAdapter returns mocked ports.PrimaryPort
+func NewPrimaryAdapter() ports.PrimaryPort {
+	return PrimaryAdapterMock{addr: "localhost"}
+}
--- test/mock/secondary.go
+++ test/mock/secondary.go
@@ -1,1 +1,37 @@
 package mock
+
+import (
+	"context"
+
+	"github.com/hanapedia/hexagon/internal/service-unit/application/ports"
+)
+
+// SecondaryAdapterMock should mock SecodaryPort from
+// "github.com/hanapedia/hexagon/internal/service-unit/application/ports"
+type SecondaryAdapterMock struct {
+	client ports.SecondaryAdapterClient
+	ports.SecondaryPortBase
+}
+
+type SecondaryAdapterClientMock struct {
+	client interface{}
+}
+
+// Call mock implementation
+func (sacm SecondaryAdapterMock) Call(context.Context) ports.SecondaryPortCallResult {
+	return ports.SecondaryPortCallResult{Payload: nil, Error: nil}
+}
+
+// Close mock implementation
+func (sacm SecondaryAdapterClientMock) Close() {
+	return
+}
+
+// NewSecondaryAdapter creates mocked implementation of ports.SecondaryPort
+func NewSecondaryAdapter() ports.SecodaryPort {
+	return &SecondaryAdapterMock{client: NewSecondaryAdapterClient()}
+}
+
+func NewSecondaryAdapterClient() ports.SecondaryAdapterClient {
+	return SecondaryAdapterClientMock{client: nil}
+}
