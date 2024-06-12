df0ec9d74e9ee604bbe5dc732a800192bf2f8956
Hiroki <hiroki11hanada@gmail.com>
2024-06-13 00:55:15 +0900

    feat: adjust onErrorSpec

--- pkg/api/v1/onerrorspec.go
+++ pkg/api/v1/onerrorspec.go
@@ -1,5 +1,23 @@
 package v1
 
+import (
+	"math"
+	"time"
+)
+
+type RetryBackoffPolicy = string
+
+const (
+	NO_BACKOFF          RetryBackoffPolicy = "none"
+	CONSTANT_BACKOFF    RetryBackoffPolicy = "constant"
+	LINEAR_BACKOFF      RetryBackoffPolicy = "linear"
+	EXPONENTIAL_BACKOFF RetryBackoffPolicy = "exponential"
+)
+
+var (
+	DEFAULT_RETRY_INITIAL_BACKOFF time.Duration = time.Millisecond
+)
+
 // OnErrorSpec is the configuration for what the service unit will do
 // when the secondary adapter call fails
 type OnErrorSpec struct {
@@ -10,34 +28,42 @@ type OnErrorSpec struct {
 	// Retry configurations
 	// RetryBackoffPolicy configurs the backoff policy used for subsequent retries.
 	// Available configurations are
-	//   - none: Default. No retries.
+	//   - none: Default. No retry backoff.
 	//   - constant: Retries without any backoffs.
 	//   - linear: Retries with linearly scaling backoff. Scaling can be configured from RetryBackoffScaling
 	//   - exponential: Retries with exponentially scaling backoff.
-	RetryPolicy string `json:"retryPolicy,omitempty"`
+	RetryBackoffPolicy RetryBackoffPolicy `json:"retryBackoffPolicy,omitempty"`
 
 	// RetryMaxAttempt specifies the max number of retries before giving up.
 	//
 	// If both RetryMaxDuration is also specified, the one that expires first will take precedence.
 	RetryMaxAttempt int `json:"retryMaxAttempt,omitempty"`
 
-	// RetryMaxDuration specifies the max duration to retry for before giving up.
-	//
-	// If both RetryMaxAttempt is also specified, the one that expires first will take precedence.
-	//
-	// Must be parsable using time.ParseDuration
-	RetryMaxDuration string `json:"retryMaxDuration,omitempty"`
-
 	// RetryInitialBackoff specifies the initial duration to backoff
 	//
 	// Must be parsable using time.ParseDuration
 	RetryInitialBackoff string `json:"retryInitialBackoff,omitempty"`
+}
 
-	// RetryBackoffScaling is the scaling factor for linear backoff policies.
-	//
-	// For linear backoff it is used as the multiplier for each backoff duration.
-	// e.g. the backoff duration for nth attmept is
-	//
-	// initial backoff + backoff scaling * (n - 1) * initial backoff
-	RetryBackoffScaling int             `json:"retryBackoffScaling,omitempty"`
+// Get parsed timeout as time.Duration
+func (oes *OnErrorSpec) GetInitialBackoff() time.Duration {
+	duration, err := time.ParseDuration(oes.RetryInitialBackoff)
+	if err != nil {
+		return DEFAULT_RETRY_INITIAL_BACKOFF
+	}
+	return duration
+}
+
+// GetNthBackoff returns the backoff duration for Nth retry attempt.
+func (oes *OnErrorSpec) GetNthBackoff(n int) time.Duration {
+	switch oes.RetryBackoffPolicy {
+	case CONSTANT_BACKOFF:
+		return oes.GetInitialBackoff()
+	case LINEAR_BACKOFF:
+		return oes.GetInitialBackoff() * time.Duration(n)
+	case EXPONENTIAL_BACKOFF:
+		return oes.GetInitialBackoff() * time.Duration(math.Pow(2, float64(n - 1)))
+	case NO_BACKOFF:
+	}
+	return 0
 }
