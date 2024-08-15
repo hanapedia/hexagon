fa1d4496ba8525f2bc71374ea0a6a1454e8e7bea
Hiroki <hiroki11hanada@gmail.com>
2024-08-15 20:33:44 +0900

    feat: reimplement rest server with net/http for better context propagation

--- internal/service-unit/infrastructure/adapters/primary/server/rest/server.go
+++ internal/service-unit/infrastructure/adapters/primary/server/rest/server.go
@@ -2,13 +2,13 @@ package rest
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"net/http"
 	"sync"
 	"time"
 
-	"github.com/gofiber/contrib/otelfiber"
-	"github.com/gofiber/fiber/v2"
 	"github.com/hanapedia/hexagon/internal/service-unit/application/core/runtime"
 	"github.com/hanapedia/hexagon/internal/service-unit/domain"
 	"github.com/hanapedia/hexagon/internal/service-unit/domain/contract"
@@ -18,27 +18,25 @@ import (
 	"github.com/hanapedia/hexagon/pkg/operator/constants"
 	"github.com/hanapedia/hexagon/pkg/operator/logger"
 	util "github.com/hanapedia/hexagon/pkg/service-unit/utils"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 // must implement ports.PrimaryPort
 type RestServerAdapter struct {
 	addr   string
-	server *fiber.App
+	server *http.Server
+	mux    *http.ServeMux
 }
 
 func NewRestServerAdapter() *RestServerAdapter {
-	app := fiber.New(fiber.Config{
-		DisableStartupMessage: true,
-	})
-
-	// enable tracing
-	if config.GetEnvs().TRACING {
-		app.Use(otelfiber.Middleware())
-	}
+	addr := config.GetRestServerAddr()
+	mux := http.NewServeMux()
+	server := &http.Server{Addr: addr, Handler: mux}
 
 	adapter := RestServerAdapter{
 		addr:   config.GetRestServerAddr(),
-		server: app,
+		server: server,
+		mux:    mux,
 	}
 
 	return &adapter
@@ -49,11 +47,16 @@ func (rsa *RestServerAdapter) Serve(ctx context.Context, wg *sync.WaitGroup) err
 	go func() {
 		<-ctx.Done()
 		logger.Logger.Infof("Context cancelled. Rest Server shutting down.")
-		rsa.server.Shutdown()
+		rsa.server.Shutdown(context.Background())
 		wg.Done()
 	}()
 
-	return rsa.server.Listen(rsa.addr)
+	err := rsa.server.ListenAndServe()
+	if err != nil && err == http.ErrServerClosed {
+		return nil
+	}
+
+	return err
 }
 
 func (rsa *RestServerAdapter) Register(handler *domain.PrimaryAdapterHandler) error {
@@ -61,71 +64,52 @@ func (rsa *RestServerAdapter) Register(handler *domain.PrimaryAdapterHandler) er
 		return errors.New(fmt.Sprintf("Invalid configuartion for handler %s.", handler.GetId()))
 	}
 
+	var handlerFunc http.Handler
+
+	handlerFunc = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// record time for logging
+		startTime := time.Now()
+		// defer log
+		defer rsa.log(r.Context(), handler, startTime)
+
+		// call tasks
+		result := runtime.TaskSetHandler(r.Context(), handler)
+		defer func() {
+			// record metrics
+			go server.ObserveServerAdapterDuration(startTime, handler.ServiceName, handler.ServerConfig, result.ShouldFail)
+		}()
+
+		if result.ShouldFail {
+			w.WriteHeader(http.StatusInternalServerError)
+			restResponse := contract.RestResponseBody{}
+			json.NewEncoder(w).Encode(restResponse)
+			return
+		}
+
+		// write response
+		payloadSize := model.GetPayloadSize(handler.ServerConfig.Payload)
+		payload := util.GenerateRandomString(payloadSize)
+
+		restResponse := contract.RestResponseBody{
+			Payload: &payload,
+		}
+
+		logger.Logger.Debugf("Ran %s, responding with %v bytes.", handler.GetId(), payloadSize)
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(restResponse)
+	})
+
+	if config.GetEnvs().TRACING {
+		handlerFunc = otelhttp.NewHandler(handlerFunc, handler.GetId())
+	}
+
 	var err error
 	switch handler.ServerConfig.Action {
 	case constants.GET, constants.READ:
-		rsa.server.Get("/"+handler.ServerConfig.Route, func(c *fiber.Ctx) error {
-			// record time for logging
-			startTime := time.Now()
-			// defer log
-			defer rsa.log(c.UserContext(), handler, startTime)
-
-			// call tasks
-			result := runtime.TaskSetHandler(c.UserContext(), handler)
-			defer func() {
-				// record metrics
-				go server.ObserveServerAdapterDuration(startTime, handler.ServiceName, handler.ServerConfig, result.ShouldFail)
-			}()
-
-			if result.ShouldFail {
-				restResponse := contract.RestResponseBody{}
-				return c.Status(fiber.StatusInternalServerError).JSON(restResponse)
-			}
-
-			// write response
-			payloadSize := model.GetPayloadSize(handler.ServerConfig.Payload)
-			payload := util.GenerateRandomString(payloadSize)
-
-			restResponse := contract.RestResponseBody{
-				Payload: &payload,
-			}
-
-			logger.Logger.Debugf("Ran %s, responding with %v bytes.", handler.GetId(), payloadSize)
-
-			return c.Status(fiber.StatusOK).JSON(restResponse)
-		})
+		rsa.mux.Handle(fmt.Sprintf("GET /%s", handler.ServerConfig.Route), handlerFunc)
 	case constants.POST, constants.WRITE:
-		rsa.server.Post("/"+handler.ServerConfig.Route, func(c *fiber.Ctx) error {
-			// record time for logging
-			startTime := time.Now()
-			// defer log
-			defer rsa.log(c.UserContext(), handler, startTime)
-
-			// call tasks
-			result := runtime.TaskSetHandler(c.UserContext(), handler)
-			defer func() {
-				// record metrics
-				go server.ObserveServerAdapterDuration(startTime, handler.ServiceName, handler.ServerConfig, result.ShouldFail)
-			}()
-
-			if result.ShouldFail {
-				restResponse := contract.RestResponseBody{}
-				return c.Status(fiber.StatusInternalServerError).JSON(restResponse)
-			}
-
-			// write response
-			payloadSize := model.GetPayloadSize(handler.ServerConfig.Payload)
-			payload := util.GenerateRandomString(payloadSize)
-
-			// write response
-			restResponse := contract.RestResponseBody{
-				Payload: &payload,
-			}
-
-			logger.Logger.Debugf("Ran %s, responding with %v bytes.", handler.GetId(), payloadSize)
-
-			return c.Status(fiber.StatusOK).JSON(restResponse)
-		})
+		rsa.mux.Handle(fmt.Sprintf("POST /%s", handler.ServerConfig.Route), handlerFunc)
 	default:
 		err = errors.New("Handler has no matching action")
 	}
