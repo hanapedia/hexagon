6ffedb10acc6721c72ebd71718aac438c5ad5674
Hiroki <hiroki11hanada@gmail.com>
2024-08-23 12:27:00 +0900

    feat: use strict unmarshall to detect unknown fields

--- pkg/operator/validation/validation.go
+++ pkg/operator/validation/validation.go
@@ -7,7 +7,7 @@ import (
 // Validate multiple service unit configs
 // serviceUnitConfigs: an array of service unit configs
 // returns validation error
-func ValidateServiceUnitConfigs(serviceUnitConfigs []model.ServiceUnitConfig) ConfigValidationError {
+func ValidateServiceUnitConfigs(serviceUnitConfigs []*model.ServiceUnitConfig) ConfigValidationError {
 	// validate service unit and adapters fields
 	var configValidationError ConfigValidationError
 	configValidationError.Extend(validateFieldsForAllServices(serviceUnitConfigs))
@@ -16,10 +16,10 @@ func ValidateServiceUnitConfigs(serviceUnitConfigs []model.ServiceUnitConfig) Co
 }
 
 // validate fields on all of the service
-func validateFieldsForAllServices(serviceUnitConfigs []model.ServiceUnitConfig) ConfigValidationError {
+func validateFieldsForAllServices(serviceUnitConfigs []*model.ServiceUnitConfig) ConfigValidationError {
 	var configValidationError ConfigValidationError
 	for _, serviceUnitConfig := range serviceUnitConfigs {
-		configValidationError.Extend(ValidateFields(&serviceUnitConfig))
+		configValidationError.Extend(ValidateFields(serviceUnitConfig))
 	}
 	return configValidationError
 }
